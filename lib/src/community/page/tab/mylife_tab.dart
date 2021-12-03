cb7640cde96025381b4338ec72ff5438f7e5b661
MMMIIIN <wbsldj7645@naver.com>
2021-12-03 15:17:02 +0900

    piechart List init

--- lib/src/community/page/tab/mylife_tab.dart
+++ lib/src/community/page/tab/mylife_tab.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_example/src/community/controller/community_controller.dart';
-import 'package:firebase_example/src/community/data/community_data.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,81 +10,70 @@ class MyLifeTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // Container(
-        //   height: 200,
-        //   child: PieChart(
-        //     PieChartData(
-        //       sections: [
-        //         PieChartSectionData(
-        //           value: 100,
-        //         ),
-        //         PieChartSectionData(
-        //           value: 150,
-        //           color: Colors.indigo
-        //         ),
-        //         PieChartSectionData(
-        //           value: 70,
-        //           color: Colors.purple
-        //         ),
-        //       ]
-        //     )
-        //   ),
-        // )
-        MaterialButton(
-          onPressed: () async {
-            _communityController.myLifeDataList.clear();
-            await FirebaseFirestore.instance
-                .collection('mylife')
-                .get()
-                .then((value) {
-              value.docs.forEach((element) {
-                print('value.size = ${element.data()}');
-                _communityController.myLifeDataList
-                    .add(MyLifeData.fromJson(element.data()));
-                // return print(element['todos'][0]['color']);
-              });
-            });
-          },
-          color: Colors.indigo,
-        ),
-        MaterialButton(
-          onPressed: () async {
-            await FirebaseFirestore.instance
-                .collection('mylife')
-                .add(MyLifeData(
-                    'good', DateTime.now(), 'mingwan', 'content Test', <Todo>[
-                  Todo(
-                    'testTodo1',
-                    3,
-                    DateTime.now(),
-                    TimeOfRange(12, 0, 18, 30),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _communityController.myLifeDataList.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(10)),
+                  padding: EdgeInsets.all(10),
+                  height: 300,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${_communityController.myLifeDataList[index].title}',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            '${_communityController.myLifeDataList[index].writer}',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w200),
+                          )
+                        ],
+                      ),
+                      Flexible(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          child: PieChart(
+                            PieChartData(
+                              centerSpaceRadius: 40,
+                              sections: List<PieChartSectionData>.generate(
+                                _communityController
+                                    .myLifeDataList[index].todos.length,
+                                (todoIndex) {
+                                  return PieChartSectionData(
+                                      value: _communityController
+                                          .myLifeDataList[index]
+                                          .todos[todoIndex]
+                                          .value
+                                          .toDouble());
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                              '${_communityController.myLifeDataList[index].content}'),
+                        ],
+                      )
+                    ],
                   ),
-                  Todo(
-                    'testTodo2',
-                    6,
-                    DateTime.now(),
-                    TimeOfRange(9, 0, 12, 30),
-                  ),
-              Todo(
-                'testTodo3',
-                2,
-                DateTime.now(),
-                TimeOfRange(10, 0, 11, 20),
-              ),
-              Todo(
-                'testTodo4',
-                1,
-                DateTime.now(),
-                TimeOfRange(18, 0, 21, 30),
-              ),
-                ]).toJson());
-          },
-          color: Colors.deepOrangeAccent,
-        ),
-        MaterialButton(onPressed: () {
-          print(_communityController.myLifeDataList.length);
-        },
-        color: Colors.purpleAccent,)
+                ),
+              );
+            },
+          ),
+        )
       ],
     );
   }
